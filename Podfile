a16b5bb8a43eb65b9cb6e914b336840323d0e25c
sauna <soneko2102@gmail.com>
2021-11-14 16:59:04 +0900

    Initial Commit

---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'testCodable' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for testCodable
+
+pod 'Alamofire'
+
+end
---
+++ testCodable/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testCodable
+//
+//  Created by KS on 2021/11/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
