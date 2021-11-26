c44ad2dada09a5122b4795f57bb8b9d1fe24c73f
sauna <soneko2102@gmail.com>
2021-11-26 14:44:08 +0900

    Codable完成

--- testCodable/ViewController.swift
+++ testCodable/ViewController.swift
@@ -10,35 +10,73 @@ import Alamofire
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var userIDLabel: UILabel!
-    @IBOutlet weak var idLabel: UILabel!
-    @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet weak var completedLabel: UILabel!
 
-    struct UsersInfo: Codable {
-        var userId: Int
-        var id: Int
-        var title: String
-        var completed: Bool
+    // API
+    //    {
+    //     "apis": {
+    //
+    //         "totalContents": { "total": 2 } ,
+    //
+    //         "feeds": [
+    //             {
+    //                 "userId": 2,
+    //                 "id": 3,
+    //
+    //             },
+    //
+    //             {
+    //                 "userId": 6,
+    //                 "id": 9,
+    //             }
+    //             ]
+    //             // feeds
+    //     }
+    //    // apis
+    //    }
+
+    struct AWSAPI: Codable {
+        let apis: APIs
+
+        struct APIs: Codable {
+            let totalContents: TotalContents
+            let feeds: [Feeds]
+        }
+
+        struct TotalContents: Codable {
+            let total: Int
+        }
+
+        struct Feeds: Codable {
+            let userId: Int
+            let id: Int
+        }
     }
+
 
     @IBAction func requestButtonTapped(_ sender: Any) {
 
-        AF.request("https://jsonplaceholder.typicode.com/todos/1") { urlRequest in }
+        AF.request("****************************************************") { urlRequest in }
         .response{ response in
 
             guard let data = response.data else { return }
-            let jsonObject = try? JSONDecoder.init().decode(UsersInfo.self, from: data)
 
-            guard let userID = jsonObject?.userId else { return }
-            guard let id = jsonObject?.id else { return }
-            guard let title = jsonObject?.title else { return }
-            guard let completed = jsonObject?.completed else { return }
+            do {
+                let json = try JSONDecoder().decode(AWSAPI.self, from: data)
+                print(json)
 
-            self.userIDLabel.text = "userID: \(userID)"
-            self.idLabel.text = "id: \(id)"
-            self.titleLabel.text = "title: \(title)"
-            self.completedLabel.text = "completed: \(completed)"
+                let total = json.apis.totalContents.total
+                print(total)
+
+                for i in 0...total - 1 {
+
+                    let userID = json.apis.feeds[i].userId
+                    let id = json.apis.feeds[i].id
+
+                    print(userID,id)
+
+                }
+
+            } catch { print(error.localizedDescription ) }
         }
     }
 }
